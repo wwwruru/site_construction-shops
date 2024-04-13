368c145fecbb1d9a2e29436d9b88b16c6567de47
Kostya <60737838+wwwruru@users.noreply.github.com>
2024-04-13 11:45:41 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -30,3 +30,40 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navWrapper = document.querySelector('.nav-wrapper');
+
+    // Функция для открытия и закрытия бургер-меню
+    function toggleBurgerMenu() {
+        navWrapper.classList.toggle('active');
+    }
+
+    // Слушатель события клика на кнопку бургер-меню
+    burgerMenu.addEventListener('click', function () {
+        toggleBurgerMenu();
+    });
+
+    // Слушатель события изменения размера окна для отображения навигации при ширине экрана более 700px
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 700) {
+            navWrapper.classList.remove('active');
+        }
+    });
+
+    // Слушатель события клика на пункты бургер-меню для их закрытия
+    navWrapper.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A') {
+            toggleBurgerMenu();
+        }
+    });
+
+    // Добавим вызов проверки при загрузке страницы
+    window.addEventListener('load', function () {
+        if (window.innerWidth > 700) {
+            navWrapper.classList.remove('active');
+        }
+    });
+});
